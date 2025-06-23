74cc9b486202834adaf82006dfa8a59d17947372
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-06-23 10:33:24 +0700

    project-ci4-portfolio

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        $config = config('MySite');
+        echo "Selamat Datang di {$config->siteName}. Kirim kritik dan saran ke email {$config->siteEmail}."; 
+    }
+}
